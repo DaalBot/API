ffb981a953c38514a7cff3349d12213dbee3dafa
NotPiny <103984634+NotPiny@users.noreply.github.com>
2024-10-16 11:13:08 +0100

    Add stats route

---
+++ routes/get/bot/stats.js
@@ -0,0 +1,48 @@
+const pantry = require('pantry-node');
+const pantryId = process.env.PANTRY_ID;
+const pantryClient = new pantry(pantryId);
+const client = require('../../../client.js');
+
+let cachedOutput = {
+    addedAt: -1,
+    data: {
+        message_counts: {
+            'day': 0,
+        },
+    }
+}
+
+module.exports = async(req, res) => {
+    if (cachedOutput.addedAt + 1000 *  5 > Date.now())
+        return res.json(cachedOutput.data);
+
+    try {
+        const currentData = await pantryClient.basket.get(`analytics${process.env.HTTP ? '-dev' : ''}`);
+        const data = currentData;
+        let messages = data.messages || [];
+
+        const now = Date.now();
+        const oneDay = 1000 * 60 * 60 * 24;
+        messages = messages.filter((message) => now - message < oneDay);
+        messages = Array.from(new Set(messages));
+
+        const dayMessages = messages.filter((message) => now - message < oneDay);
+        cachedOutput = {
+            addedAt: Date.now(),
+            data: {
+                message_counts: {
+                    'day': dayMessages.length,
+                },
+                bot: {
+                    user_count: client.users.cache.size,
+                    channel_count: client.channels.cache.size,
+                    guild_count: client.guilds.cache.size
+                }
+            }
+        }
+
+        res.json(cachedOutput.data);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while fetching the data' });
+    }
+}
