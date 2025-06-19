ba943e61f453d99aa7e8345cf7d3bf21648c080e
NotPiny <103984634+NotPiny@users.noreply.github.com>
2025-06-19 21:56:00 +0100

    Allow for api to make request to itself over localhost instead of having to leave network

--- src/lib/tools/_internal.ts
+++ src/lib/tools/_internal.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 
 export async function get(path: string, headers?: Record<string, string>, raw?: boolean): Promise<string | AxiosResponse> {
-    const response = await axios.get(`https://bot.daalbot.xyz${path}`, {
+    const response = await axios.get(`http${process.env.DEV ? 's://bot.daalbot.xyz' : '://localhost:3000'}${path}`, {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': process.env.BotCommunicationKey,
@@ -14,7 +14,7 @@ export async function get(path: string, headers?: Record<string, string>, raw?: 
 }
 
 export async function post(path: string, data: any = {}, headers?: Record<string, string>): Promise<string> {
-    const response = await axios.post(`https://bot.daalbot.xyz${path}`, data, {
+    const response = await axios.post(`http${process.env.DEV ? 's://bot.daalbot.xyz' : '://localhost:3000'}${path}`, data, {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': process.env.BotCommunicationKey,
@@ -25,7 +25,7 @@ export async function post(path: string, data: any = {}, headers?: Record<string
 }
 
 export async function deleteRequest(path: string, headers?: Record<string, string>): Promise<string> {
-    const response = await axios.delete(`https://bot.daalbot.xyz${path}`, {
+    const response = await axios.delete(`http${process.env.DEV ? 's://bot.daalbot.xyz' : '://localhost:3000'}${path}`, {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': process.env.BotCommunicationKey,
