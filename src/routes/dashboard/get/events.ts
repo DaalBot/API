0b3c335a77b729bc63f87dda6d9635d4675b60d8
NotPiny <103984634+NotPiny@users.noreply.github.com>
2026-02-19 19:05:11 +0000

    Right we gotta debug

--- src/routes/dashboard/get/events.ts
+++ src/routes/dashboard/get/events.ts
@@ -30,10 +30,13 @@ export async function exec(req: Request, res: Response) {
     const id = req.query.id;
 
     let eventsMasterResult: string | object = await tools.database.read(`/events/events.json`);
+    console.log(eventsMasterResult);
     if (typeof eventsMasterResult === 'object') {
         eventsMasterResult = JSON.stringify(eventsMasterResult);
     }
+    console.log(eventsMasterResult);
     let eventsMaster = JSON.parse(eventsMasterResult) as unknown as Array<{ id: string, name: string, description: string, on: string, guild: string, enabled: boolean }>;
+    console.log(JSON.stringify(eventsMaster));
     eventsMaster = eventsMaster.filter(e => e.guild === req.query.guild);
     if (eventsMaster.length === 0) return res.status(404).json({ ok: false, error: 'Event not found' });
 
