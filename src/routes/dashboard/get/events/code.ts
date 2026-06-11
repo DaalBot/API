3a05e5573eb6c588f984cee7e5e3e28e6fb903c7
NotPiny <103984634+NotPiny@users.noreply.github.com>
2026-06-12 00:10:54 +0100

    Remove potentially overworking regex

--- src/routes/dashboard/get/events/code.ts
+++ src/routes/dashboard/get/events/code.ts
@@ -60,14 +60,14 @@ export async function exec(req: Request, res: Response) {
         let code = file;
         const fileLines = file.split('\n');
         const startLine = fileLines.findIndex(line => line.match(/execute:\s*\(async\(.*, util\) => {/));
-        const endLine = fileLines.length; // Exclude the last line (closing bracket of the module.exports object)
+        const endLine = fileLines.length - 2; // Exclude the last line (closing bracket of the module.exports object)
         
         // Extract the code between the start and end lines
-        code = fileLines.slice(startLine + 1, endLine).join('\n');
+        code = fileLines.slice(startLine + 1).join('\n');
         // Remove the first line of the code (the async function declaration)
         code = code.replace(/^\s*async\s*\(message, util\) => {\s*/, '');
-        // Remove the last line of the code (the closing bracket of the async function)
-        code = code.replace(/\s*}\s*$/, '');
+        // // Remove the last line of the code (the closing bracket of the async function)
+        // code = code.replace(/\s*}\s*$/, '');
 
         return code;
     } catch (e) {
